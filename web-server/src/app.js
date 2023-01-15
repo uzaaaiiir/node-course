ec1fda483877b084285ab38b774b3e00a182499d
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-01-15 18:29:53 -0500

    Serving static files using Express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,32 +1,21 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-// app.com
-const port = 3000;
+app.use(express.static(publicDirectoryPath));
 
 // get() determines what information to serve up when a url is called
-app.get("", (req, res) => {
-    res.send("<h1>Hello Express</h1>");
-});
 
-app.get("/help", (req, res) => {
-    res.send([
-        {
-            name: "Uzair",
-            age: 24,
-            location: "Toronto, ON",
-        },
-        {
-            name: "John",
-            age: 23,
-        },
-    ]);
-});
+// Will never run if index.html is provided
+// app.get("", (req, res) => {
+//     res.send("<h1>Hello Express</h1>");
+// });
 
-app.get("/about", (req, res) => {
-    res.send("<h1>About</h1>");
-});
+// app.get("/about", (req, res) => {
+//     res.send("<h1>About</h1>");
+// });
 
 app.get("/weather", (req, res) => {
     res.send({
@@ -35,6 +24,6 @@ app.get("/weather", (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}.`);
+app.listen(3000, () => {
+    console.log(`Server is up on port ${3000}.`);
 });
