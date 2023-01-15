245b120648bbc08c64ea078cbc2671b9b23aae70
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-01-14 22:09:18 -0500

    Enhanced object literals, destructuring objects

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,33 @@
+// Object property shorthand
+
+const name = "Andrew";
+const userAge = 24;
+const location = "Toronto, ON";
+
+const user = {
+    name,
+    userAge,
+    location,
+};
+
+console.log(user);
+
+// Destructuring
+const product = {
+    label: "Red notebook",
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+};
+
+const { label: productLabel, price, stock, salePrice = 15 } = product;
+console.log(productLabel);
+console.log(price);
+console.log(stock);
+console.log(salePrice);
+
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock);
+};
+
+transaction("order", product);
