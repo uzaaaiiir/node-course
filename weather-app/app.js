59984ff304d71b85d13db897020eec5e8f3b4a58
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-01-14 22:09:36 -0500

    Updated weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,20 +3,25 @@
 const geocode = require("./utils/geocode.js");
 const forecast = require("./utils/forecast");
 
-geocode("Philly", (error, data) => {
-    if (error) {
-        return console.log(error);
-    }
+const address = process.argv[2];
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+    console.log("Please provide an address.");
+} else {
+    geocode(address, (error, data) => {
         if (error) {
             return console.log(error);
         }
 
-        console.log(data.location);
-        console.log(forecastData);
-        console.log(
-            `${forecastData.weatherDescription}. It is currently ${forecastData.temperature} degrees out. It feels like ${forecastData.feelsLike} degrees out.`
-        );
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+
+            console.log(data.location);
+            console.log(
+                `${forecastData.weatherDescription}. It is currently ${forecastData.temperature} degrees out. It feels like ${forecastData.feelsLike} degrees out.`
+            );
+        });
     });
-});
+}
