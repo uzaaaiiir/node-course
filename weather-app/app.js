c8c8db01310c3937ae3524fed23a5f258db80926
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-01-14 19:24:12 -0500

    Lat/Longitude Request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,31 +1,38 @@
 "use strict";
 
-// Synchronous Programming - goes in order from top to bottom
-// console.log("Starting");
-
-// setTimeout(() => {
-//     console.log("2 Second Timer");
-// }, 2000);
-
-// setTimeout(() => {
-//     console.log("0 Second Timer");
-// }, 0);
-
-// console.log("Stopping");
-
 const request = require("postman-request");
 
 const url =
-    "http://api.weatherstack.com/current?access_key=0353de7307be4eee4eb9e53769f0b567&query=Toronto";
+    "http://api.weatherstack.com/current?access_key=0353de7307be4eee4eb9e53769f0b567&query=Empire%20State%20Building&units=f";
 
 request({ url: url, json: true }, (error, response) => {
-    // const data = JSON.parse(response.body);
-    // console.log(data);
-    // console.log(data.current);
+    if (error) {
+        console.log("Unable to connect to weather service!");
+    } else if (response.body.error) {
+        console.log("Unable to find location.");
+    } else {
+        const data = response.body.current;
+        console.log(
+            `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`
+        );
+    }
+});
 
-    const data = response.body.current;
-    console.log(data);
-    console.log(
-        `It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`
-    );
+// Geocoding
+// Address -> Lat/Long -> Weather
+const geocodeURL =
+    "https://us1.locationiq.com/v1/search?key=pk.48e5fe92d6629a18152f39424ad4f237&q=Empire%20State%20Building&format=json&limit=1";
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    if (error) {
+        console.log("Unable to connect to weather service!");
+    } else if (response.body.error) {
+        console.log("Unable to find location.");
+    } else {
+        const data = response.body[0];
+        const location = data.display_name.split(",")[0];
+        console.log(
+            `The latitude and longitude of ${location} is: Lat ${data.lat} and Long ${data.lon}`
+        );
+    }
 });
