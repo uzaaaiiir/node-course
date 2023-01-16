5fc19b76e4aad9286ba97fd0a00324b24b86c265
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-01-15 19:30:36 -0500

    Using templating engines

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,18 +4,29 @@ const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
-// get() determines what information to serve up when a url is called
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather Appp",
+        name: "Uzair Mohiuddin",
+    });
+});
 
-// Will never run if index.html is provided
-// app.get("", (req, res) => {
-//     res.send("<h1>Hello Express</h1>");
-// });
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About Me",
+        name: "Uzair Mohiuddin",
+    });
+});
 
-// app.get("/about", (req, res) => {
-//     res.send("<h1>About</h1>");
-// });
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help",
+        helpMessage: "Here's where you find help.",
+    });
+});
 
 app.get("/weather", (req, res) => {
     res.send({
