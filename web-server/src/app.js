1c2660fd1638eb1a47fce59e09bc437df0fe968a
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-01-23 21:45:03 -0500

    Setup routing for Weather query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
     res.render("index", {
-        title: "Weather Appp",
+        title: "Weather App",
         name: "Uzair Mohiuddin",
     });
 });
@@ -40,10 +40,30 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: "You must provide an address",
+        });
+    }
+
+    console.log(req.query.address);
     res.send({
-        forecast: 24,
-        location: "Toronto",
+        address: req.query.address,
     });
+});
+
+app.get("/products", (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: "You must provide a search term.",
+        });
+    }
+
+    res.send({
+        products: [],
+    });
+    console.log(req);
+    console.log(req.query);
 });
 
 // 404 for any help route not found
