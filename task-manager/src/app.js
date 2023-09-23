8f479ad2a93a8148d7b291a3a7a10a051b94144c
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-09-22 22:07:19 -0400

    Setup test environment

---
+++ task-manager/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+// connect to db
+require("./db/mongoose"); // ensures the file runs
+
+// routers
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+module.exports = app;
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,18 +1,6 @@
-const express = require("express");
+const app = require("./app");
 
-// connect to db
-require("./db/mongoose"); // ensures the file runs
-
-// routers
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
-
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 // start server
 app.listen(port, () => {
