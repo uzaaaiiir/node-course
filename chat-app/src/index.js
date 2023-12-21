3eca5159a803f78dab5a2a1b87b28815879ed906
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-12-20 21:37:05 -0500

    Socket IO: Setting up socket io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,17 +1,35 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "..", "public");
 
 app.use(express.static(publicDirectoryPath));
 
+let count = 0;
+
+io.on("connection", (socket) => {
+    console.log("New WebSocket Connection");
+
+    socket.emit("countUpdated", count);
+
+    socket.on("increment", () => {
+        count++;
+        // socket.emit("countUpdated", count);
+        io.emit("countUpdated", count);
+    });
+});
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(publicDirectoryPath, "index.html"));
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
